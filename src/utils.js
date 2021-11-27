c17c19fb737d59ea2b84387baaec39f5d3af55e8
fisker Cheung <lionkay@gmail.com>
2021-11-27 15:45:50 +0800

    Use arrow functions

--- src/utils.js
+++ src/utils.js
@@ -8,16 +8,14 @@ const encodeName = (name) =>
     character.codePointAt(0).toString(16).padStart(2, '0'),
   )
 
-function decode(text) {
-  return new Map(
+const decode = (text) =>
+  new Map(
     text
       .match(/[A-Z]{3}[^A-Z]+/g)
       .map((text) => [text.slice(0, 3), decodeName(text.slice(3))]),
   )
-}
 
-function encode(currencies) {
-  return currencies.map(({code, name}) => `${code}${encodeName(name)}`).join('')
-}
+const encode = (currencies) =>
+  currencies.map(({code, name}) => `${code}${encodeName(name)}`).join('')
 
 export {encode, decode}
